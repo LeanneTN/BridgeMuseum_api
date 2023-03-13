98d0e65f09593828cec2a8bccff59f449df2b76b
LeanneTN <2321604632@qq.com>
2023-03-13 21:11:46 +0800

    feat(common): common response and response code

--- src/main/java/com/example/bridgemuseum_api/common/CommonResponse.java
+++ src/main/java/com/example/bridgemuseum_api/common/CommonResponse.java
@@ -1,4 +1,50 @@
 package com.example.bridgemuseum_api.common;
 
-public class CommonResponse {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+
+/**
+ * @author Leanne
+ * data: 2023/3/13
+ */
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CommonResponse<T>{
+    private Integer code;
+    private String message;
+    private T data;
+
+    protected CommonResponse(Integer code, String message, T data){
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    @JsonIgnore
+    public boolean isSuccess(){
+        return this.code == ResponseCode.SUCCESS.getCode();
+    }
+
+    public static <T> CommonResponse<T> createForSuccess(){
+        return new CommonResponse<>(ResponseCode.SUCCESS.getCode(), ResponseCode.SUCCESS.getDescription(), null);
+    }
+
+    public static <T> CommonResponse<T> createForSuccess(T data){
+        return new CommonResponse<>(ResponseCode.SUCCESS.getCode(), ResponseCode.SUCCESS.getDescription(), data);
+    }
+
+    public static <T> CommonResponse<T> createForError(){
+        return new CommonResponse<>(ResponseCode.ERROR.getCode(), ResponseCode.ERROR.getDescription(), null);
+    }
+
+    // set error message
+    public static <T> CommonResponse<T> createForError(String message){
+        return new CommonResponse<>(ResponseCode.ERROR.getCode(), message, null);
+    }
+
+    // set error code and message
+    public static <T> CommonResponse<T> createForError(Integer code, String message){
+        return new CommonResponse<>(code, message, null);
+    }
 }
--- src/main/java/com/example/bridgemuseum_api/common/ResponseCode.java
+++ src/main/java/com/example/bridgemuseum_api/common/ResponseCode.java
@@ -1,4 +1,21 @@
 package com.example.bridgemuseum_api.common;
 
-public class ResponseCode {
+import lombok.Getter;
+
+@Getter
+public enum ResponseCode {
+    //response success
+    SUCCESS(0, "SUCCESS"),
+    //response error
+    ERROR(1, "ERROR"),
+    ARGUMENT_ILLEGAL(2, "ARGUMENT_ILLEGAL"),
+    NEED_LOGIN(3, "NEED LOGIN");
+
+    private final int code;
+    private final String description;
+
+    ResponseCode(int code, String description){
+        this.code = code;
+        this.description = description;
+    }
 }
