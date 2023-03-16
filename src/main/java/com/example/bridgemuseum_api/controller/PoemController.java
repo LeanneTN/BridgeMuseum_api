9efedfab38afcf7e46c6de1f0265e7dc9c2b8b30
LeanneTN <2321604632@qq.com>
2023-03-16 19:12:36 +0800

    feat: finish basic interfaces of the poem entity

--- src/main/java/com/example/bridgemuseum_api/controller/PoemController.java
+++ src/main/java/com/example/bridgemuseum_api/controller/PoemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 @RestController
 @RequestMapping("/poem")
@@ -22,5 +23,35 @@ public class PoemController {
     @PutMapping("/")
     public CommonResponse<Poem> addPoem(@RequestBody @NotBlank Poem poem){
         return poemService.addPoem(poem);
+    }
+
+    @GetMapping("/poems/name/{name}")
+    public CommonResponse<List<Poem>> getPoemsByName(@PathVariable("name") String name){
+        return poemService.getPoemsByName(name);
+    }
+
+    @GetMapping("/poems/poet/{poet}")
+    public CommonResponse<List<Poem>> getPoemsByPoet(@PathVariable("poet") String poet){
+        return poemService.getPoemsByPoet(poet);
+    }
+
+    @GetMapping("/poems/dynasty/{dynasty}")
+    public CommonResponse<List<Poem>> getPoemsByDynasty(@PathVariable("dynasty") String dynasty){
+        return poemService.getPoemsByDynasty(dynasty);
+    }
+
+    @PostMapping("/")
+    public CommonResponse<Poem> modifyPoem(@RequestBody @NotBlank Poem poem){
+        return poemService.modifyPoem(poem);
+    }
+
+    @DeleteMapping("/id/{id}")
+    public CommonResponse<Poem> deletePoemById(@PathVariable("id") Integer id){
+        return poemService.deletePoemById(id);
+    }
+
+    @DeleteMapping("/")
+    public CommonResponse<Poem> deletePoemByPoem(@RequestBody @NotBlank Poem poem){
+        return poemService.deletePoemByPoem(poem);
     }
 }
